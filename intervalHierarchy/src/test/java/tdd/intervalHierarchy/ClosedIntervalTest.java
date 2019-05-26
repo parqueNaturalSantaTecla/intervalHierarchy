74a613698f7ddaddf008f24ccab0a186e71f6973
setil <setil@DESKTOP-PEHM5QR>
2019-05-26 10:20:07 +0200

    Historia Usuario 1. Prueba añadida

--- intervalHierarchy/src/test/java/tdd/intervalHierarchy/ClosedIntervalTest.java
+++ intervalHierarchy/src/test/java/tdd/intervalHierarchy/ClosedIntervalTest.java
@@ -7,9 +7,16 @@ import junit.framework.TestCase;
 public class ClosedIntervalTest extends TestCase {
 
 	@Test
-	public void testIntersected() {
+	public void testIntersectedOverlappedOnLeft() {
 		ClosedInterval one = new ClosedIntervalBuilder().min(3).max(14).build();
 		ClosedInterval another = new ClosedIntervalBuilder().min(1).max(7).build();
+		assertTrue(one.isIntersected(another));
+	}
+	
+	@Test
+	public void testIntersectedInside() {
+		ClosedInterval one = new ClosedIntervalBuilder().min(3).max(14).build();
+		ClosedInterval another = new ClosedIntervalBuilder().min(5).max(7).build();
 		assertTrue(one.isIntersected(another));
 	}
 }
