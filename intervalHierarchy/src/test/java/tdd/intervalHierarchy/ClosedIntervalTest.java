d9f921fd13ca204c17b4a20308ad01cff6bc4f2c
setil <setil@DESKTOP-PEHM5QR>
2019-05-26 10:33:16 +0200

    Historia usuario 1. Prueba añadida

--- intervalHierarchy/src/test/java/tdd/intervalHierarchy/ClosedIntervalTest.java
+++ intervalHierarchy/src/test/java/tdd/intervalHierarchy/ClosedIntervalTest.java
@@ -40,4 +40,11 @@ public class ClosedIntervalTest extends TestCase {
 		ClosedInterval another = new ClosedIntervalBuilder().min(1).max(2).build();
 		assertFalse(one.isIntersected(another));
 	}
+	
+	@Test
+	public void testIsNotIntersectedOnRight() {
+		ClosedInterval one = new ClosedIntervalBuilder().min(3).max(14).build();
+		ClosedInterval another = new ClosedIntervalBuilder().min(17).max(22).build();
+		assertFalse(one.isIntersected(another));
+	}
 }
